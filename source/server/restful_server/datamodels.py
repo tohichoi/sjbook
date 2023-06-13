96a5f5fa6dfa683c6ecd082f53602084a57b345c
tohichoi@naver.com <tohichoi@naver.com>
2023-06-14 02:09:40 +0900

    updated

--- source/server/restful_server/datamodels.py
+++ source/server/restful_server/datamodels.py
@@ -17,10 +17,12 @@ class TransactionStat:
         # self.qs: QuerySet = queryset
         self.min_date = min_date
         self.max_date = max_date
-        self.stat = self.calc_stat(queryset) if calc_stat else None
+        self.qs = queryset
+        self.stat = self.calc_stat(self.qs) if calc_stat else None
 
-    def calc_stat(self, qs):
+    def calc_stat(self):
         # 은행 계좌별로 합계를 구한다
+        qs = self.qs
         stat = qs.values('bank', 'bank__alias').order_by('bank').annotate(
             sum_withdraw=Sum('withdraw'),
             sum_saving=Sum('saving')).annotate(sum_profit=F('sum_saving') - F('sum_withdraw'))
@@ -35,4 +37,5 @@ class TransactionStat:
         stat = []
         for i in l:
             stat.append(TransactionBankStat(i['bank'], i['bank__alias'], i['sum_withdraw'], i['sum_saving'], i['sum_profit']))
+        self.stat = stat
         return stat
